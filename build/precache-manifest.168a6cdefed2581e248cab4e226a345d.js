a4c3344f7fc92c57c3cea0d045257ea3ef55b987
Le Richter <lerichter.c@gmail.com>
2020-10-23 20:18:13 -0300

    Projeto5-WhatsLab

---
+++ build/precache-manifest.168a6cdefed2581e248cab4e226a345d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b5e30490c4be55240d575be36e1c9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "032e779f1534982e3428",
+    "url": "/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "d2ab91b3429e99c6664d",
+    "url": "/static/js/2.5b233331.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.5b233331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032e779f1534982e3428",
+    "url": "/static/js/main.d5346481.chunk.js"
+  },
+  {
+    "revision": "8468388d8de162d1ff20",
+    "url": "/static/js/runtime-main.74bcd271.js"
+  },
+  {
+    "revision": "3ff5d7350e66d5abf5d9d6da71a746e9",
+    "url": "/static/media/avatar.3ff5d735.png"
+  },
+  {
+    "revision": "3be45fa38ec31445cf7edfd22f14d6c4",
+    "url": "/static/media/dot.3be45fa3.svg"
+  },
+  {
+    "revision": "1cb7545f161211e60743a9f2e790e448",
+    "url": "/static/media/search.1cb7545f.svg"
+  }
+]);
